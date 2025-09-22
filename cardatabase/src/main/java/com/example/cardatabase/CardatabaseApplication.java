b63ab63f4ebd71f074aac94cc5c9f632de783469
maybeags <suren0709@gmail.com>
2025-09-22 11:50:26 +0900

    feat: before h2

---
+++ cardatabase/src/main/java/com/example/cardatabase/CardatabaseApplication.java
@@ -0,0 +1,18 @@
+package com.example.cardatabase;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardatabaseApplication {
+	private static final Logger logger = LoggerFactory.getLogger(
+			CardatabaseApplication.class
+	);
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardatabaseApplication.class, args);
+		logger.info("Application Started ! / 애플리케이션이 실행되었습니다.");
+	}
+}

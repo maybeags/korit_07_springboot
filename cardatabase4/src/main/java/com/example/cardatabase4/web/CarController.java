95aabbe02b058d3797913510697da567f3c551a5
maybeags <suren0709@gmail.com>
2025-09-29 15:39:17 +0900

    feat: CRUD based on Entity Car finished

--- cardatabase4/src/main/java/com/example/cardatabase4/web/CarController.java
+++ cardatabase4/src/main/java/com/example/cardatabase4/web/CarController.java
@@ -30,4 +30,30 @@ public class CarController {
 
         return new ResponseEntity<>(savedCar, HttpStatus.CREATED);
     }
+
+    // 3. 차량 한 대 조회
+    @GetMapping("/cars/{id}")
+    public ResponseEntity<Car> getCarById(@PathVariable Long id) {
+        return carService.getCarById(id)
+                .map(car -> ResponseEntity.ok().body(car))  // 차량 정보가 있으면 200 OK를 반환할 것
+                .orElse(ResponseEntity.notFound().build()); // 없으면 404 Not Found를 return 시킬 것
+    }
+
+    // 4. 차량 한 대 삭제
+    @DeleteMapping("/cars/{id}")
+    public ResponseEntity<Void> deleteCar(@PathVariable Long id) {
+        if(carService.deleteCar(id)) {
+            return ResponseEntity.noContent().build();  // 삭제 성공시 204 No Content return
+        } else {
+            return ResponseEntity.notFound().build();   // 대상이 없으면 404 Not Found
+        }
+    }
+
+    // 5. 차량 한 대 수정
+    @PutMapping("/cars/{id}")
+    public ResponseEntity<Car> updateCar(@PathVariable Long id, @RequestBody Car carDetails) {
+        return carService.updateCar(id, carDetails)
+                .map(updateCar -> ResponseEntity.ok().body(updateCar)) // 수정 성공 시에는 200 OK
+                .orElse(ResponseEntity.notFound().build()); // 수정할 차량 id 검색 실패시 404 Not Found
+    }
 }

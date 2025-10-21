b881ddba6b0860592049f5fd7cbfb2b03105a56a
maybeags <suren0709@gmail.com>
2025-10-21 11:14:04 +0900

    feat: car dummy data uploaded

--- cardatabase_2/src/main/java/com/example/cardatabase/SecurityConfig.java
+++ cardatabase_2/src/main/java/com/example/cardatabase/SecurityConfig.java
@@ -54,13 +54,19 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .cors(withDefaults())
-                .sessionManagement(sessionManagement ->
-                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorizeHttpRequests ->
-                        authorizeHttpRequests.requestMatchers(HttpMethod.POST, "/login").permitAll().anyRequest().authenticated())
-                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .exceptionHandling(exceptionHandling ->
-                        exceptionHandling.authenticationEntryPoint(exceptionHandler));
+                        authorizeHttpRequests.anyRequest().permitAll());
+
+
+//        http.csrf(csrf -> csrf.disable())
+//                .cors(withDefaults())
+//                .sessionManagement(sessionManagement ->
+//                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .authorizeHttpRequests(authorizeHttpRequests ->
+//                        authorizeHttpRequests.requestMatchers(HttpMethod.POST, "/login").permitAll().anyRequest().authenticated())
+//                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
+//                .exceptionHandling(exceptionHandling ->
+//                        exceptionHandling.authenticationEntryPoint(exceptionHandler));
         return http.build();
     }
 
